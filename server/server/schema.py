b320b021e93b6f2cae460862cd568a56740f4b7b
Joon Young Lee <joonlee7890@gmail.com>
2021-03-08 18:22:54 -0500

    Began adding mutations as a setup for testing a simple test with graphene

--- server/server/schema.py
+++ server/server/schema.py
@@ -2,6 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from server.app.types import UserType, GroupType, PostType, CommentType, MatchType, HeartType
+from server.app.mutations import RegisterUser
 
 class Query(graphene.ObjectType):
   login = graphene.Field(
@@ -12,5 +13,8 @@ class Query(graphene.ObjectType):
     UserType,
     username = graphene.String()
   )
+  
+class Mutations(graphene.ObjectType):
+  register_user = RegisterUser.Field()
 
-schema = graphene.Schema(query = Query)
+schema = graphene.Schema(query = Query, mutation = Mutations)
